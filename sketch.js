dc9c3937468d314d85c228285be1906efe3ee36b
Jeremi Kiermasz <kiermaszjeremi@gmail.com>
2024-03-14 13:46:40 +0000

    error fix

--- sketch.js
+++ sketch.js
@@ -1,4 +1,4 @@
-//global for the controls and input 
+//global for the controls and input
 var controls = null;
 //store visualisations in a container
 var vis = null;
@@ -8,52 +8,55 @@ var sound = null;
 var fourier;
 
 function preload() {
-	// sound = loadSound('assets/stomper_reggae_bit.mp3');
-	// sound = loadSound('assets/aerodynamic.mp3');
-	// sound = loadSound('assets/Voyager.mp3');
-	// sound = loadSound('assets/nirvana.flac');
-	sound = loadSound('assets/Blow Me Away.mp3');
+  // sound = loadSound('assets/stomper_reggae_bit.mp3');
+  // sound = loadSound('assets/aerodynamic.mp3');
+  // sound = loadSound('assets/Voyager.mp3');
+  // sound = loadSound('assets/nirvana.flac');
+  sound = loadSound("assets/Blow Me Away.mp3");
+
+  font = loadFont("assets/inconsolata.regular.ttf");
 }
 
 function setup() {
-	 canvas = createCanvas(windowWidth, windowHeight, WEBGL);
-	 canvas.position(0,0);
-	 background(0);
-	 controls = new ControlsAndInput();
+  canvas = createCanvas(windowWidth, windowHeight, WEBGL);
+  canvas.position(0, 0);
+  background(0);
+  textFont(font);
+  controls = new ControlsAndInput();
 
-	 //instantiate the fft object
-	 fourier = new p5.FFT();
+  //instantiate the fft object
+  fourier = new p5.FFT();
 
-	 //create a new visualisation container and add visualisations
-	 vis = new Visualisations();
-	 vis.add(new Spectrum());
-	 vis.add(new WavePattern());
-	 vis.add(new Needles());
-	 vis.add(new Shapes());
+  //create a new visualisation container and add visualisations
+  vis = new Visualisations();
+  vis.add(new Spectrum());
+  vis.add(new WavePattern());
+  vis.add(new Needles());
+  vis.add(new Shapes());
 }
 
 function draw() {
-	background(0);
-	//draw the selected visualisation
-	translate(-width/2, -height/2);
-	vis.selectedVisual.draw();
-	//draw the controls on top.
-	controls.draw();
+  background(0);
+  //draw the selected visualisation
+  translate(-width / 2, -height / 2);
+  vis.selectedVisual.draw();
+  //draw the controls on top.
+  controls.draw();
 }
 
 function mouseClicked() {
-	controls.mousePressed();
+  controls.mousePressed();
 }
 
 function keyPressed() {
-	controls.keyPressed(keyCode);
+  controls.keyPressed(keyCode);
 }
 
-//when the window has been resized. Resize canvas to fit 
+//when the window has been resized. Resize canvas to fit
 //if the visualisation needs to be resized call its onResize method
 function windowResized() {
-	resizeCanvas(windowWidth, windowHeight);
-	if (vis.selectedVisual.hasOwnProperty('onResize')) {
-		vis.selectedVisual.onResize();
-	}
+  resizeCanvas(windowWidth, windowHeight);
+  if (vis.selectedVisual.hasOwnProperty("onResize")) {
+    vis.selectedVisual.onResize();
+  }
 }
